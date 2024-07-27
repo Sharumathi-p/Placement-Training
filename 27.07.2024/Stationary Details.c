f1b6df6e2999df132a0bfd62fd0bc1f1e5d86ec0
Sharumathi-p <154426223+Sharumathi-p@users.noreply.github.com>
2024-07-27 05:28:08 -0700

    Create Stationary Details.c

---
+++ 27.07.2024/Stationary Details.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+unoin d
+{
+  char name[20]
+  int qty;
+  float price;
+}o;
+void main()
+{
+  scanf("%s",o.name);
+  printf("Name: %s\n",o.name);
+  scanf("%d",&o.qty);
+  printf("Qty: %d\n",o.qty);
+  scanf("%f",&o.price);
+  printf("Price: %.2f\n",o,price);
+}

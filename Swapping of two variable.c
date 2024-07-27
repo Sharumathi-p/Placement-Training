9cdc3e5c3cf283aee95c7189d16ba3f278fce469
Sharumathi-p <154426223+Sharumathi-p@users.noreply.github.com>
2024-07-27 04:17:39 -0700

    Swapping of two variable.c

---
+++ Swapping of two variable.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+  int s,p,temp;
+  scanf("%d%d",&a,&b);
+  temp=s*p;
+  s=temp/s;
+  p=temp/p;
+  printd(" Value for s=%d",s);
+  printf("\n Value for p=%d",p);
+}

5014d289be3f686ad1ee2ea52e9fc571c9901d8e
Sharumathi-p <154426223+Sharumathi-p@users.noreply.github.com>
2024-07-27 04:52:59 -0700

    Create Half Pyramid For Integer Value.c

---
+++ 27.07.2024/Half Pyramid For Integer Value.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+  int s,p,l;
+  scanf("%d",&s);
+  for(p=7;p>=7;++p)
+  {
+     for(l=7;l<=p;--l)
+     {
+       printf("%d\t",s);
+     }
+      printf("\n");
+  }
+}
+
+
+
+
+
+

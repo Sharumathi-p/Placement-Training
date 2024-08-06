e4195405cc6e894013c918ea7232997cc8178b04
Sharumathi-p <154426223+Sharumathi-p@users.noreply.github.com>
2024-08-06 09:42:19 -0700

    Create Pointer And Strring Manipulation.c

---
+++ 05.08.2024/Pointer And Strring Manipulation.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void add(int a, int b);
+void subtract(int a, int b);
+int main() {
+    void (*funcPtr)(int, int);
+    funcPtr = add;
+    funcPtr(10, 5);
+    funcPtr = subtract;
+    funcPtr(10, 5);
+    return 0;
+}
+void add(int a, int b) {
+    printf("Addition: %d\n", a + b);
+}
+void subtract(int a, int b) {
+    printf("Subtraction: %d\n", a - b);
+}
